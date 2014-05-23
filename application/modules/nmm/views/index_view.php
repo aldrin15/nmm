51a9f0c1410f82d2dc9a624b4942b7ea8d6f4b7e
aldrin15 <dioaldrin15@gmail.com>
2014-05-23 08:11:03 -0800

    This is a test update for home page

--- application/modules/nmm/views/index_view.php
+++ application/modules/nmm/views/index_view.php
@@ -1,1 +1,35 @@
 <?php $this->load->view('header_content')?>
+
+<style type="text/css">
+.error {color:#ff0000;}
+
+search-option ul li {float:left;}
+</style>
+
+
+<div class="home">
+	<div class="search-option">
+		<form action="" method="post">
+			<ul>
+				<li>
+					<label for="From">From: </label>
+					<input type="text" name="from" id=""/>
+				</li>
+				<li>
+					<label for="To">To: </label>
+					<input type="text" name="to" id=""/>
+				</li>
+				<li>
+					<input type="text" name="" id="datepicker"/>
+				</li>
+				<li>
+					<input type="submit" name="ride_submit" value="Search"/>
+				</li>
+			</ul>
+			
+			<div class="clr"></div>
+		</form>
+	</div>
+</div>
+
+<script type="text/javascript"></script>
