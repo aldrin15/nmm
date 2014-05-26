08ba8843ae287e18175897345f44013bc08bbc7e
aldrin15 <dioaldrin15@gmail.com>
2014-05-25 18:58:39 -0800

    added new files

---
+++ application/modules/contact/views/contact_successfull_view.php
@@ -0,0 +1,10 @@
+<?php $this->load->view('header_content')?>
+<br /><br /><br />
+
+<div class="contact">
+	<h1>Contact Us Page</h1>
+	
+	<div>
+		<p>You have successfully sent your inquiry</p>
+	</div>
+</div>
