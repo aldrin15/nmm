78ba396916ee70a80fe917c1f1fe2702d1e678ce
aldrin15 <dioaldrin15@gmail.com>
2014-07-11 10:00:07 +0800

    Adjusted feedback view layout, Change home page layout

--- application/modules/feedback/views/feedback_view.php
+++ application/modules/feedback/views/feedback_view.php
@@ -1,24 +1,22 @@
 <style type="text/css">
-.feedback {text-align:center; margin:20px 0;}
+.feedback { font-size:2em; text-align:center; /*padding:100px;*/ position:relative; height:180px;}
+.feedback .feedback-message:first-child { display:block; }
+.feedback .feedback-message { position:absolute; display:none; width:100%;}
 .feedback-message span {display:block; text-align:left; margin-top:10px;}
 .feedback .fl {margin-left:10px;}
 .feedback .f-left {vertical-align:top}
 .feedback .f-right {vertical-align:top; margin-top:25px;}
-.feedback-message p {position:relative; background: #e6e6e6; border-radius:5px; -webkit-border-radius:5px; -moz-border-radius:5px; -ms-border-radius:5px; padding:10px 10px; width:450px;}
+.feedback-message p {position:relative; /*background: #e6e6e6;*/ font-style:italic; border-radius:5px; -webkit-border-radius:5px; -moz-border-radius:5px; -ms-border-radius:5px; padding:10px 10px; /*width:450px;*/}
 .feedback-message img {position:absolute; bottom:-9px; left:5px;}
 </style>
 <div class="feedback">
-	<div class="f-left fl"><img src="<?php echo base_url('assets/images/quote/quote_left.jpg')?>" width="34" height="28" alt=""/></div>
 	<?php foreach($feedback_data as $row):?>
-	<div class="feedback-message fl">
-		<p>
-			<?php echo $row['comment']?>
-			<img src="<?php echo base_url('assets/images/quote/arrow_bottom.jpg')?>" width="12" height="9" alt=""/>
-		</p>
-		<span><?php echo $row['firstname'].' '.$row['lastname']?></span>
+	<div class="feedback-message">
+		<p><?php echo $row['comment']?></p>
+		
+		<p>by <?php echo $row['firstname'].' '.$row['lastname']?></p>
 	</div>
 	<?php endforeach?>
-	<div class="f-right fl"><img src="<?php echo base_url('assets/images/quote/quote_right.jpg')?>" width="34" height="28" alt=""/></div>
 	
 	<div class="clr"></div>
 </div>
--- application/modules/nmm/views/index_view.php
+++ application/modules/nmm/views/index_view.php
@@ -1,11 +1,35 @@
 <?php $this->load->view('header_content')?>
-		
+<style type="text/css">
+.event-detail p {background:#000; width:190px;}
+
+.video {padding-top:55px;}
+.home-features {font-size:1.3em; margin:70px auto; width:710px;}
+.home-features h3 {text-align:center; text-transform:uppercase;}
+.home-features ul li:nth-child(2n+2) {float:right;}
+.home-features ul li {float:left; margin-bottom:50px; width:300px;}
+.home-features ul li i {float:left; margin-right:10px;}
+.home-features ul li:nth-child(1) i {display:block; background: url('<?php echo base_url('assets/images/page_template/home_icon.png')?>') no-repeat; width:71px; height:70px;}
+.home-features ul li:nth-child(2) i {display:block; background: url('<?php echo base_url('assets/images/page_template/home_icon.png')?>') no-repeat 0 -70px; width:71px; height:70px;}
+.home-features ul li:nth-child(3) i {display:block; background: url('<?php echo base_url('assets/images/page_template/home_icon.png')?>') no-repeat 0 -140px; width:71px; height:70px;}
+.home-features ul li:nth-child(4) i {display:block; background: url('<?php echo base_url('assets/images/page_template/home_icon.png')?>') no-repeat 0 -210px; width:71px; height:70px;}
+
+.subscription {color:#678222; text-align:center; margin:90px 0;}
+.subscription h3 {text-transform:uppercase; color:#000;}
+.subscription a {color:#678222;}
+.subscription a:hover {text-decoration:none;}
+.subscription div {background:#e9fdb5; display:inline-block; text-align:center; border:1px solid #ccc; border-radius:5px; -webkit-border-radius:5px; -moz-border-radius:5px; vertical-align:top; padding:10px; min-height:130px;}
+.subscription div:hover {background:#d6fd75;}
+.subscription div span {display:block;}
+.subscription div span:nth-child(2) {font-size:3.5em;}
+</style>
+	
 		<div class="slideshow-search" style="position:relative;">
 			<div class="slideshow">			
 				<ul class="rslides" id="slider1">
 					<li><img src="<?php echo base_url('assets/images/slideshow/1.jpg')?>" alt=""></li>
 					<li><img src="<?php echo base_url('assets/images/slideshow/2.jpg')?>" alt=""></li>
 					<li><img src="<?php echo base_url('assets/images/slideshow/3.jpg')?>" alt=""></li>
+					<li><img src="<?php echo base_url('assets/images/slideshow/4.jpg')?>" alt=""></li>
 				</ul>
 				
 				<div class="clr"></div>
@@ -31,82 +55,151 @@
 				Tjen og spar penge med ligesindede,<br />
 				alt imens du hjælper miljøet.
 				</p>
-				
-				<a href="#" class="btn-gray">Join us now!</a>
 			</aside>
 			
-			<div class="events-quote fr">
-				<div id="events" class="carousel slide">
-					<!-- Carousel items -->
-					<div class="carousel-inner">
-						<div class="active item">
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+			<div class="fr video span5">
+				<a href="#"><img src="<?php echo base_url('assets/images/video.jpg')?>" width="410" height="173" alt=""/></a>
+			</div>
+			
+			<div class="clr"></div>
+			
+			<div class="home-features">
+				<h3>All the features you want</h3>
+				
+				<ul>
+					<li><i></i>You can create your own lift<br /> or you can wish for a lift. <div class="clr"></div></li>
+					<li><i></i>You can create an Events. <div class="clr"></li>
+					<li><i></i>You can invite people/friends<br /> for a ride. <div class="clr"></li>
+					<li><i></i>You can share it via Facebook. <div class="clr"></li>
+				</ul>
+				
+				<div class="clr"></div>
+			</div>
+			
+			<div class="subscription">
+				<h3>Start by choosing your plan</h3>
+				
+				<div class="span2">
+					<a href="javascript:void(0)">
+						<span>14 DAYS FREE TRIAL</span>
+						<span>FREE</span>
+					</a>
+				</div>
+				<div class="span2">
+					<a href="javascript:void(0)">
+						<span>MONTHLY</span>
+						<span>3.99</span>
+						<span>EURO</span>
+					</a>
+				</div>
+				<div class="span2">
+					<a href="javascript:void(0)">
+						<span>6 MONTHS</span>
+						<span>12.99</span>
+						<span>EURO</span>
+					</a>
+				</div>
+				<div class="span2">
+					<a href="javascript:void(0)">
+						<span>12 MONTHS</span>
+						<span>18.99</span>
+						<span>EURO</span>
+					</a>
+				</div>
+				
+				<span class="clr" style="display:block;"></span>
+			</div>
+			
+			<?php echo modules::run('feedback')?>
+			
+			<div id="events" class="carousel slide">
+				<!-- Carousel items -->
+				<div class="carousel-inner">
+					<div class="active item">
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>The Late Night Show</p>
 							</div>
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>The Early Morning Show</p>
 							</div>
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>First Concert of John Doe</p>
 							</div>
-							
-							<div class="clr"></div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>Second Concert of John Doe</p>
+							</div>
 						</div>
 						
-						<div class="item">
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+						<div class="clr"></div>
+					</div>
+					
+					<div class="item">
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/page_template/blank_profile_large.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>John Doe</p>
 							</div>
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/page_template/blank_profile_large.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>Michael Jordan</p>
 							</div>
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/page_template/blank_profile_large.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>Connor Kenway</p>
 							</div>
-							
-							<div class="clr"></div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/page_template/blank_profile_large.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>John Connor</p>
+							</div>
 						</div>
 						
-						<div class="item">
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
+						<div class="clr"></div>
+					</div>
+					
+					<div class="item">
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/dummy_car.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>This is a test</p>
 							</div>
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
-							</div>
-							<div class="span2">
-								<img src="<?php echo base_url('assets/images/events/events.jpg')?>" width="163" height="141" alt=""/>
-								<div class="event-detail">
-									<p>This is a test</p>
-								</div>
-							</div>
-							
-							<div class="clr"></div>
 						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/dummy_car.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>This is a test</p>
+							</div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/dummy_car.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>This is a test</p>
+							</div>
+						</div>
+						<div class="span2">
+							<img src="<?php echo base_url('assets/images/dummy_car.jpg')?>" width="200" height="190" alt=""/>
+							<div class="event-detail">
+								<p>This is a test</p>
+							</div>
+						</div>
+						
+						<div class="clr"></div>
 					</div>
 				</div>
 				
@@ -118,10 +211,6 @@
 					<li><a href="#2"></a></li>
 					<li><a href="#3"></a></li>
 				</ul>
-				
-					<div class="clr"></div>
-				
-				<?php echo modules::run('feedback')?>
 			</div>
 		</div>
 		
@@ -162,6 +251,15 @@ $(function () {
 		
 		$('.carousel-linked-nav li:eq(' + idx + ')').addClass('active'); // select currently active item and add active class
 	});
+	
+	showNextQuote();
 });
+
+var quoteIndex = -1;
+
+function showNextQuote() {
+	++quoteIndex;
+	$(".feedback-message").eq(quoteIndex % $(".feedback-message").length).fadeIn(1000).delay(5000).fadeOut(1000, showNextQuote);
+}
 </script>
 <?php echo modules::run('lift/auto_suggest_city')?>
