c67a023bd35d07665a8c967dcba19fc90d188321
aldrin15 <dioaldrin15@gmail.com>
2014-07-12 12:06:17 +0800

    added front-end validation in jquery

--- application/modules/contact/views/contact_view.php
+++ application/modules/contact/views/contact_view.php
@@ -12,7 +12,7 @@
 				<div class="clr"></div>
 			</li>
 			<li>
-				<label for="Email">Email: <?php echo form_error('email', '<span class="error">', '</span>')?></label>
+				<label for="Email" class="err-email">Email: <?php echo form_error('email', '<span class="error">', '</span>')?></label>
 				<input type="text" name="email" id="" class="form-control"/>
 				
 				<div class="clr"></div>
@@ -31,3 +31,45 @@
 	
 	<div class="clr"></div>
 </div>
+
+<script type="text/javascript">
+$(function() {
+	$('input[name="contact_submit"]').click(function() {
+		var name	= $('input[name="name"]'),
+			email	= $('input[name="email"]'),
+			message	= $('textarea[name="message"]'),
+			error 	= 0;
+
+		$('.err-email').html('Email: ');
+		$('*').removeClass('error-bd');
+		var regx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		
+		if(name.val() == '') {
+			name.addClass('error-bd');
+			error = 1;
+		}
+		
+		if(email.val() == '') {
+			email.addClass('error-bd');
+			error = 1;
+		} else {
+			if(!regx.test(email.val())) {
+				email.addClass('error-bd');
+				$('.err-email').html('Email: <span class="error">Please enter valid email address</span>');
+				error = 1;
+			}
+		}
+		
+		if(message.val() == '') {
+			message.addClass('error-bd');
+			error = 1;
+		}
+		
+		if(error == 0) {
+			$(this).submit();
+		} else {
+			return false;
+		}
+	});
+});
+</script>
